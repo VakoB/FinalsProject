0adb132a77f6dc85f82387175259d20f186fc3fb
VakoB <vladimer.butkhuzi.1@btu.edu.ge>
2023-01-04 21:58:27 +0400

    registration, log in, repair password ვიზუალურად მოვამზადე

---
+++ app/src/main/java/com/example/shoppingapp/fragments/ForgotPasswordFragment.kt
@@ -0,0 +1,22 @@
+package com.example.shoppingapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.shoppingapp.R
+
+
+class ForgotPasswordFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/shoppingapp/fragments/RegistrationFragment.kt
@@ -0,0 +1,23 @@
+package com.example.shoppingapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.shoppingapp.R
+
+
+class RegistrationFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
