96038c43441734fcfabbd96bfbd0d407421bebec
VakoB <vladimer.butkhuzi.1@btu.edu.ge>
2023-01-06 23:45:28 +0400

    .

--- app/src/main/java/com/example/shoppingapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppingapp/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
